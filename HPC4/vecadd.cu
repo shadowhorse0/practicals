b1d254fa03ac2c85248606b009d30941fc94427b
vaibhav padghan <vaibhav.111padghan@gmail.com>
2023-05-30 15:01:41 +0530

    little improvement

--- HPC4/vecadd.cu
+++ HPC4/vecadd.cu
@@ -1,103 +1,69 @@
 #include<bits/stdc++.h>
 using namespace std;
-// CUDA kernel for vector addition
+
 __global__
-void vectorAddition(const int* A, const int* B, int* C, int size)
-{
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
-
-    if (index < size)
-    {
-        C[index] = A[index] + B[index];
+void vectorAdd(int* a, int* b, int* c, int size){
+    int index = blockIdx.x+blockDim.x+threadIdx.x;
+    if(index<size){
+        c[index]=a[index]+b[index];
     }
 }
 
-// Function to perform vector addition sequentially
-void sequentialVectorAddition(const int* A, const int* B, int* C, int size)
-{
-    for (int i = 0; i < size; ++i)
-    {
-        C[i] = A[i] + B[i];
+void seqVecAdd(int *a, int* b, int* c,int size){
+    for(int i=0; i<size; i++){
+        c[i]=a[i]+b[i];
     }
 }
 
-int main()
-{
-    const int size = 1000000;  // Size of the vectors
+int main(){
+    int size = 10000;
+    int* a = new int[size];
+    int* b = new int[size];
+    int* c = new int[size];
 
-    // Allocate memory for vectors on host
-    int* A = new int[size];
-    int* B = new int[size];
-    int* C = new int[size];
-
-    // Initialize vectors with random values
-    for (int i = 0; i < size; ++i)
-    {
-        A[i] = rand() % 100;
-        B[i] = rand() % 100;
+    for(int i=0; i<size; i++){
+        a[i]=rand()%100;
+        b[i]=rand()%100;
     }
 
-    // Allocate memory for vectors on device
-    int* d_A, * d_B, * d_C;
-    cudaMalloc((void**)&d_A, sizeof(int) * size);
-    cudaMalloc((void**)&d_B, sizeof(int) * size);
-    cudaMalloc((void**)&d_C, sizeof(int) * size);
+    int *d_a,*d_b,*d_c;
+    cudaMalloc((void**)&d_a,sizeof(int)*size);
+    cudaMalloc((void**)&d_b,sizeof(int)*size);
+    cudaMalloc((void**)&d_c,sizeof(int)*size);
 
-    // Copy input vectors from host to device
-    cudaMemcpy(d_A, A, sizeof(int) * size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_B, B, sizeof(int) * size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_a,a,sizeof(int)*size,cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b,b,sizeof(int)*size,cudaMemcpyHostToDevice);
 
-    // Set up thread configuration
-    int threadsPerBlock = 256;
-    int blocksPerGrid = (size + threadsPerBlock - 1) / threadsPerBlock;
-    // Start timer for parallel algorithm
-    auto startParallel = chrono::high_resolution_clock::now();
-
-    // Launch kernel for vector addition in parallel
-    vectorAddition<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, size);
-
-    // Wait for kernel to finish execution
+    int threads = 256;
+    int blocks = (size+threads-1)/threads;
+    auto start1 = chrono::high_resolution_clock::now();
+    vectorAdd<<<blocks,threads>>>(d_a,d_b,d_c,size);
     cudaDeviceSynchronize();
+    auto stop1 = chrono::high_resolution_clock::now();
 
-    // End timer for parallel algorithm
-    auto endParallel = chrono::high_resolution_clock::now();
+    cudaMemcpy(c,d_c,sizeof(int)*size,cudaMemcpyDeviceToHost);
 
-    // Copy result vector from device to host
-    cudaMemcpy(C, d_C, sizeof(int) * size, cudaMemcpyDeviceToHost);
+    auto start2 = chrono::high_resolution_clock::now();
+    seqVecAdd(a,b,c,size);
+    auto stop2 = chrono::high_resolution_clock::now();
 
-    // Start timer for sequential algorithm
-    auto startSequential = chrono::high_resolution_clock::now();
+    auto p_time = chrono::duration_cast<chrono::microseconds>(stop1-start1);
+    auto s_time = chrono::duration_cast<chrono::microseconds>(stop2-start2);
 
-    // Perform vector addition sequentially
-    sequentialVectorAddition(A, B, C, size);
+    cout<<"Sequential Algorithm time: "<<s_time.count()<<endl;
+    cout<<"Parallel Algorithm time: "<<p_time.count()<<endl;
 
-    // End timer for sequential algorithm
-    auto endSequential = chrono::high_resolution_clock::now();
-
-    // Calculate elapsed time for parallel algorithm
-    auto durationParallel = chrono::duration_cast<chrono::microseconds>(endParallel - startParallel);
-
-    // Calculate elapsed time for sequential algorithm
-    auto durationSequential = chrono::duration_cast<chrono::microseconds>(endSequential - startSequential);
-
-    // Print performance results
-    cout << "Parallel Algorithm Time: " << durationParallel.count() << " microseconds" << endl;
-    cout << "Sequential Algorithm Time: " << durationSequential.count() << " microseconds" << endl;
-
-    // Print the results of vector addition (first 10 elements)
-    scout << "Vector Addition Result:" << endl;
-    for (int i = 0; i < 10; ++i)
-    {
-        scout << A[i] << " + " << B[i] << " = " << C[i] << endl;
+    for(int i=0; i<10; i++){
+        cout<<a[i]<<" + "<<b[i]<<" = "<<c[i]<<endl;
     }
+    
+    delete[] a;
+    delete[] b;
+    delete[] c;
 
-    // Free memory
-    delete[] A;
-    delete[] B;
-    delete[] C;
-    cudaFree(d_A);
-    cudaFree(d_B);
-    cudaFree(d_C);
+    cudaFree(d_a);
+    cudaFree(d_b);
+    cudaFree(d_c);
 
     return 0;
 }
